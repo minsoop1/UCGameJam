6ad116fbf4d73368c24060ceb35f426a0e53a589
MVP <minsoop@uchicago.edu>
2026-01-16 22:38:19 -0600

    Player and Minion

---
+++ Player.py
@@ -0,0 +1,32 @@
+import pygame
+
+SCREEN_WIDTH = 809
+SCREEN_HEIGHT = 500
+POS = (100, 100)
+
+class Zombie(pygame.sprite.Sprite):
+
+    def __init__(self, name, health, speed, sprite_image) -> None:
+        super().__init__()
+        self.name = name
+        self.health = health
+        self.speed = speed
+        self.image = pygame.image.load(sprite_image)
+        self.rect = self.image.get_rect()
+
+    def update(self, inputs) -> None:
+        if inputs[pygame.MOUSEBUTTONDOWN]:
+            pass
+            
+
+
+    def take_damage(self, amount) -> None:
+        self.health -= amount
+        if self.health <= 0:
+            self.kill()
+            
+    
+
+
+
+
